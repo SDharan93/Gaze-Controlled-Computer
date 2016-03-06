752be2912498e79c52e1e2b03115cc710753b366
jibinjacob09 <jibinjj@yahoo.ca>
2016-03-06 15:04:11 -0500

    improved code stability

--- samples/cropping/crop.cpp
+++ samples/cropping/crop.cpp
@@ -15,8 +15,16 @@ using namespace cv;
 CascadeClassifier face_cascade;
 CascadeClassifier eye_cascade;
 
+//ONLY USE IF NEED TO TROUBLESHOOT
+//#define DEBUG 1
+
 
 void DynamicCrop(){
+
+  #ifdef DEBUG
+    cout<<"Inside DynamicCrop()" << endl;
+  #endif
+
   Mat image, gray_image, face_image, eye_image;
   std::vector <Rect> face_rect;  // a vector to store face coordinates
   std::vector <Rect> eye_rect;  // a vector to store face coordinates
@@ -24,31 +32,70 @@ void DynamicCrop(){
   VideoCapture capture;
 
   capture.open(0);
-  //Exit if stream is not working
+
+  // keepin the image display size constant
   namedWindow( "Eye", WINDOW_NORMAL );
 
-
+  //Exit if stream is not working
   if(!capture.isOpened())   { cout << "Capture is not working" << endl; return ;}
   else{
+
+    #ifdef DEBUG
+      cout<<"Capture is open and working" << endl;
+    #endif
+
     for(;;)   {
       capture >> image;
 
       if(image.empty()) {break ;}
 
+      #ifdef DEBUG
+        cout<<"image successfully captured" << endl;
+      #endif
 
       cvtColor( image, gray_image, COLOR_BGR2GRAY ); //converting image into gray_image
-      //equalizeHist( gray_image, gray_image ); //equalizing the histogram of gray_image
+
+      #ifdef DEBUG
+        cout<<"image conv to grayscale" << endl;
+      #endif
+
+      equalizeHist( gray_image, gray_image ); //equalizing the histogram of gray_image
 
       //identifying the face and croping the original image
-      face_cascade.detectMultiScale( gray_image, face_rect, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(100, 100) );
-      face_image = gray_image(Rect(face_rect[0].x, face_rect[0].y, face_rect[0].width, face_rect[0].height));
+      try{
+          face_cascade.detectMultiScale( gray_image, face_rect, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(100, 100) );
+          face_image = gray_image(Rect(face_rect[0].x, face_rect[0].y, face_rect[0].width, face_rect[0].height));
+        }
+      catch (...){
+        #ifdef DEBUG
+          cout<<"Exception caught for face detection/crop" << endl;
+        #endif
+      }
+
+      #ifdef DEBUG
+        cout<<"face detection done, and face is cropped" << endl;
+      #endif
 
       //identifying the eye and cropping the face image
-      eye_cascade.detectMultiScale( face_image, eye_rect, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
-      eye_image = face_image(Rect(eye_rect[0].x, eye_rect[0].y, eye_rect[0].width, eye_rect[0].height));
+      try{
+        eye_cascade.detectMultiScale( face_image, eye_rect, 1.1, 2, 0|CASCADE_SCALE_IMAGE, Size(30, 30) );
+        eye_image = face_image(Rect(eye_rect[0].x, eye_rect[0].y, eye_rect[0].width, eye_rect[0].height));
+      }
+      catch(...){
+        #ifdef DEBUG
+          cout<<"Exception caught for eye detection/crop" << endl;
+        #endif
+      }
+
+      #ifdef DEBUG
+        cout<<"eye detection done, and eye is cropped" << endl;
+      #endif
 
       //Resizing the cropped eye image to twice the size
       resize(eye_image, eye_image, eye_image.size()*2, 0,0, INTER_NEAREST);
+      #ifdef DEBUG
+        cout<<"successfully zoomed into croped eye" << endl << endl;
+      #endif
 
       imshow("Eye", eye_image);
       if(waitKey(5) >= 0) {  break; }
@@ -66,7 +113,7 @@ void StaticCrop(){
 
   image = imread("./screenshot.jpg", 1); //reading the original image from the file
 
-  if (!image.empty()) {cout << "read the image" << endl;}
+  if (image.empty()) {cout << "IMAGE NOT FOUND" << endl; return;}
 
   cvtColor( image, gray_image, COLOR_BGR2GRAY ); //converting image into gray_image
   equalizeHist( gray_image, gray_image ); //equalizing the histogram of gray_image
@@ -85,6 +132,7 @@ void StaticCrop(){
 
   imshow("gray", eye_image);
 
+
   waitKey(0);
 }
 
@@ -92,10 +140,17 @@ void StaticCrop(){
 
 int main(int argc, char** argv){
 
+  #ifdef DEBUG
+      cout << "debug enabled" << endl;
+  #endif
+
   //attempting to load the HAAR cascades for face and eye detection
   if ((!face_cascade.load("./haarcascade_frontalface_alt.xml") ) || (!eye_cascade.load("./haarcascade_eye_tree_eyeglasses.xml") ))
   {     cout << "ERROR loading the the classifiers";    return 0;   }
 
+  #ifdef DEBUG
+      cout << "done loading cascades" << endl;
+  #endif
 
 
   //Call DynamicCrop to perform face and then eyedetcion on live camera stream
