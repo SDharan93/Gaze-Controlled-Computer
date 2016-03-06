9d3470e14cab77ba2631fa83783cef4340323f7e
Shane Dharan <ShaneDharan@gmail.com>
2016-03-06 01:09:18 -0500

    Created stub for sample code on cropping and pupil detection

---
+++ samples/pupil_detection/test.cpp
@@ -0,0 +1,15 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+using namespace cv; 
+
+int main(int argc, char** argv) {
+    cout << "This is a stub" << endl;
+    
+}
