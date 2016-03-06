46ba95cae75735cdc8f3dcc9158d070b6f32fe98
Shane Dharan <ShaneDharan@gmail.com>
2016-03-06 04:29:53 -0500

    Added pupil detection

--- samples/pupil_detection/test.cpp
+++ samples/pupil_detection/test.cpp
@@ -12,20 +12,63 @@ using namespace cv;
 int main(int argc, char** argv) {
     string sourceName = "eye.jpg";
     string sourceDisplay = "Source image";
+    string inversedSourceDisplay = "Inversed Source Image";
+    string thresholdDisplay = "Threshold Image";
     
     Mat src = imread(sourceName);
+    Mat graySource; 
+    Mat thresholdImage;
+    VideoCapture cap;
+    cap.open(0);
+
     //If the source image could not load...
-    if(!src.data) {
-       cout << "ERROR: could not load: " << sourceName << endl; 
+    if(!cap.isOpened()) {
+       cout << "ERROR: could not open camera" << endl; 
        return -1;
     }
-
-    namedWindow(sourceDisplay, CV_WINDOW_AUTOSIZE);
-    imshow(sourceDisplay, src);
-    cout << "Press any key to exit" << endl;
     
-    //Add input keys here
-    waitKey(0);
+    else {
+        //The '~' inverts the image. 
+        cvtColor(~src, graySource, CV_BGR2GRAY);
 
+        //Calculate Threshold of the image.
+        threshold(graySource, thresholdImage, 220, 255, THRESH_BINARY);
+
+        //Remove the noise in the image
+        //Find all the contours
+        vector<vector<Point> > contours;
+        findContours(thresholdImage.clone(), contours, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_NONE);
+
+        //Fill in the holes for each contour 
+        drawContours(thresholdImage, contours, -1, CV_RGB(255, 255, 255), -1);
+
+        //go through every circle to look for the pupil
+        for(int i = 0; i < contours.size(); i++) {
+            double area = contourArea(contours[i]);
+            Rect rect = boundingRect(contours[i]);
+            int radius = rect.width/2;
+
+            //look for round shapes
+            if(area >= 30 &&
+                abs(1 - ((double)rect.width / (double)rect.height)) <= 0.2 &&
+                abs(1 - (area / (CV_PI * pow(radius,2)))) <= 0.2)
+            {
+                circle(src, Point(rect.x + radius, rect.y + radius), radius, CV_RGB(255, 0, 0),2); 
+            }
+        }
+
+        namedWindow(inversedSourceDisplay, CV_WINDOW_AUTOSIZE);
+        namedWindow(sourceDisplay, CV_WINDOW_AUTOSIZE);
+        namedWindow(thresholdDisplay, CV_WINDOW_AUTOSIZE);
+
+        imshow(sourceDisplay, src);
+        imshow(inversedSourceDisplay, graySource);
+        imshow(thresholdDisplay, thresholdImage);
+
+        cout << "Press any key to exit" << endl;
+    
+        //Add input keys here
+        waitKey(0);
+    }
     return 0;
 }
