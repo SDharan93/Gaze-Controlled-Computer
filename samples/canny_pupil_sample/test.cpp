7d7341885259057f17902f32fd82297ecefe1508
Shane Dharan <ShaneDharan@gmail.com>
2016-03-09 02:25:01 -0500

    found center of blob

--- samples/canny_pupil_sample/test.cpp
+++ samples/canny_pupil_sample/test.cpp
@@ -11,10 +11,14 @@ using namespace std;
 
 bool openCamera(VideoCapture cap);
 int userInput(int input);
+void thresh_callback(int, void*);
+
+//Global Variables
+Mat dilate_image;
 
 int main( int argc, char** argv ) {
     VideoCapture cap;
-    Mat src, video_src, gray_video_src, threshold_image, edge, dilate_image;
+    Mat src, video_src, gray_video_src, threshold_image, edge, blur;
     int input = 1;
     int dilation_size = 2;
 
@@ -28,6 +32,7 @@ int main( int argc, char** argv ) {
     namedWindow("Gray", 1);
     namedWindow("Threshold", 1);
     namedWindow("Canny",1);
+    namedWindow("blur", 1);
     namedWindow("Dilation",1);
 
     while(input != 0) {
@@ -38,25 +43,62 @@ int main( int argc, char** argv ) {
         cvtColor(~video_src, gray_video_src, CV_BGR2GRAY);
         threshold(gray_video_src, threshold_image, 225, 255, THRESH_BINARY);
 
+        //blur image to elimnate unwanted edges.
+        medianBlur(threshold_image, blur, 3);
+
         //use the canny filter
-        Canny(gray_video_src, edge, 50, 150, 3);
+        Canny(blur, edge, 50, 150, 3);
 
         Mat element = getStructuringElement(2, Size(2 * dilation_size + 1, 2 * dilation_size + 1),
                   Point(dilation_size, dilation_size));     // dilation_type = MORPH_ELLIPSE
-        dilate(edge, dilate_image, element);
-        //dilate canny filtered image
-        //dilate(edge, dilate_image, NULL, 4);
+        //dilate(edge, dilate_image, element);
+
+        //morph to replace dilate
+        cv::morphologyEx(edge, dilate_image, MORPH_CLOSE, cv::noArray(),cv::Point(-1,-1),2);
+        thresh_callback(0,0);
 
         //show normal video instance
         imshow("Video", video_src);
         imshow("Gray", gray_video_src);
         imshow("Threshold", threshold_image);
         imshow("Canny", edge);
+        imshow("blur", blur);
         imshow("Dilation", dilate_image);
         input = userInput(waitKey(10));
     }
 
     return 0;
+}
+
+void thresh_callback(int, void*)
+{
+    vector<vector<Point> > contours;
+    vector<Vec4i> hierarchy;
+
+    // Find contours
+    findContours(dilate_image, contours, hierarchy, CV_RETR_TREE, CV_CHAIN_APPROX_SIMPLE, Point(0, 0));
+
+    // Get the moments
+    vector<Moments> mu(contours.size());
+    for(int i = 0; i < contours.size(); i++) {
+        mu[i] = moments(contours[i], false);
+    }
+
+    // Get the mass centers
+    vector<Point2f> mc(contours.size());
+    for(int i = 0; i < contours.size(); i++) {
+        mc[i] = Point2f(mu[i].m10/mu[i].m00 , mu[i].m01/mu[i].m00);
+    }
+
+    // Draw mass centers
+    Mat drawing = Mat::zeros(dilate_image.size(), CV_8UC1);
+    for( int i = 0; i< contours.size(); i++ ) {
+        Scalar color = Scalar(255, 255, 255);
+        line(drawing, mc[i], mc[i], color, 1, 8, 0);
+    }
+
+    namedWindow("Step 3: Mass Centers", CV_WINDOW_AUTOSIZE);
+    imshow("Step 3: Mass Centers", drawing);
 }
 
 bool openCamera(VideoCapture cap) {
