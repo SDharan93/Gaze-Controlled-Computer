8b088030ef205c7b509f47c2cd2c424c346a6d45
Shane Dharan <ShaneDharan@gmail.com>
2016-03-09 01:44:06 -0500

    eliminated skin for easier edge detection

--- samples/canny_pupil_sample/test.cpp
+++ samples/canny_pupil_sample/test.cpp
@@ -14,8 +14,9 @@ int userInput(int input);
 
 int main( int argc, char** argv ) {
     VideoCapture cap;
-    Mat src, video_src;
+    Mat src, video_src, gray_video_src, threshold_image, edge, dilate_image;
     int input = 1;
+    int dilation_size = 2;
 
     cap.open(0);
     if(!cap.isOpened()) {  // check if we succeeded
@@ -24,13 +25,34 @@ int main( int argc, char** argv ) {
     //if the user wants to use a debug image
     src = imread(argv[1]);
     namedWindow("Video",1);
+    namedWindow("Gray", 1);
+    namedWindow("Threshold", 1);
+    namedWindow("Canny",1);
+    namedWindow("Dilation",1);
 
     while(input != 0) {
-        //switch case used to handle the users input
+        //stream video instance into matrix
         cap >> video_src;
-        
 
+        //turn video instance into gray image
+        cvtColor(~video_src, gray_video_src, CV_BGR2GRAY);
+        threshold(gray_video_src, threshold_image, 225, 255, THRESH_BINARY);
+
+        //use the canny filter
+        Canny(gray_video_src, edge, 50, 150, 3);
+
+        Mat element = getStructuringElement(2, Size(2 * dilation_size + 1, 2 * dilation_size + 1),
+                  Point(dilation_size, dilation_size));     // dilation_type = MORPH_ELLIPSE
+        dilate(edge, dilate_image, element);
+        //dilate canny filtered image
+        //dilate(edge, dilate_image, NULL, 4);
+
+        //show normal video instance
         imshow("Video", video_src);
+        imshow("Gray", gray_video_src);
+        imshow("Threshold", threshold_image);
+        imshow("Canny", edge);
+        imshow("Dilation", dilate_image);
         input = userInput(waitKey(10));
     }
 
