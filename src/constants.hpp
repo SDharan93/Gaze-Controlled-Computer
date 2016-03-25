8cc5a50cfe087b0f193e85a74ca23e201e3d2424
Shane Dharan <ShaneDharan@gmail.com>
2016-03-25 16:17:11 -0400

    Added constants

---
+++ src/constants.hpp
@@ -0,0 +1,20 @@
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/objdetect.hpp>
+
+//Contains the global variables for the program. 
+using namespace cv; 
+using namespace std; 
+
+//Window Names 
+static String IMAGE_WINDOW_NAME = "Image Window";
+static String VIDEO_WINDOW_NAME = "Video Window";
+static String GRAY_WINDOW_NAME = "Gray Video Window";
+static String EYE_WINDOW_NAME = "Eye Window"; 
+static String FACE_WINDOW_NAME = "Face Window";
+
+static int CLOSE_PROGRAM = 27; //esc
+static int DEBUG_WINDOWS = 100; //d
+static int CLOSE_WINDOWS = 99; //c
--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,52 @@
 #include <iostream>
 #include <cmath>
 
+#include "constants.hpp"
+
 using namespace cv;
 using namespace std; 
 
+void initWindows();
+void displayWindows();
+int userInput(int input);
+
+Mat source;
+
 int main(int argc, char** argv) {
+    int input = 0;
+
+    //Check if the user put any image input 
+    if(argc > 0) {
+        source = imread(argv[1]);    
+    }
+    
+    initWindows();
+    while(input != CLOSE_PROGRAM) {
+        displayWindows();
+        input = userInput(waitKey(10));
+    }
 
     return 0;
+}
+
+void initWindows() {
+    namedWindow(IMAGE_WINDOW_NAME, 1);
+}
+
+void displayWindows() {
+   imshow(IMAGE_WINDOW_NAME, source); 
+}
+
+int userInput(int input) {
+    switch(input) {
+        case 27: { //User has hit esc key
+            return 27;
+        }
+        case 99: { //User has hit the c key 
+            return 99;
+        }
+        case 100: { //User has hit d key 
+            return 100;
+        }
+    }
 }
