34c5bc9a53292783c1bc324f3d4b94ec96f53da4
Shane Dharan <ShaneDharan@gmail.com>
2016-04-03 18:48:46 -0400

    removed temp files and added write to file functionality

--- src/pupilLoc.cpp
+++ src/pupilLoc.cpp
@@ -62,7 +62,7 @@ void PupilLoc::highlightPupil() {
     vector< vector<Point> > contours; 
     vector<Vec4i> hierarchy; 
     vector<Vec3f> circles;
-    fstream myfile;
+    fstream file;
     int largest_area = 0;
     int largest_contour_index = 0; 
     Rect bounding_rect;
@@ -83,6 +83,9 @@ void PupilLoc::highlightPupil() {
     int radius = bounding_rect.width/2;
     circle(ref_image, Point(bounding_rect.x+radius, bounding_rect.y+radius), radius, CV_RGB(255,0,0),2);
     cout << "POINTS: X= " << bounding_rect.x+radius << " Y= " << bounding_rect.y+radius << endl;
+    file.open("coordinates.txt", fstream::out | fstream::trunc);
+    file << "POINTS: X= " << bounding_rect.x+radius << " Y= " << bounding_rect.y+radius << endl;
+    file.close();
 }
 
 void PupilLoc::display_windows() {
